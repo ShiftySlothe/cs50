00768173e63bba00209e7c63a44e7d0558c75990
ShiftySlothe <ShiftySlothe@users.noreply.github.com>
2021-02-26 09:44:26 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -5,28 +5,33 @@ int main(void)
 {
     int sizeOfTower;
     
-    //This solution appears to work 
+    //Requests input until corect 
     do{
-      sizeOfTower = get_int("How big do you want your tower? ");
+      sizeOfTower = get_int("How tall do you want your tower? (1 - 8)");
     }
-    while(sizeOfTower > 9 || sizeOfTower < 0);
+    while(sizeOfTower > 8 || sizeOfTower < 0);
 
+    //For each row
     for (int i = 1; i <= sizeOfTower; i++)
     {
+        //Draw spaces
         for (int j = sizeOfTower - i - 1; j >= 0; j--)
         {
             printf(" ");
         }
-
+        //Draw blocks
         for(int k = i; k > 0; k--)
         {
             printf("#");
         }
+        //Print 2 spaces 
         printf("  ");
+        //Draw same number of blocks
         for(int l = i; l > 0; l--)
         {
             printf("#");
         }
+        //Go to the next line
         printf("\n");
     }
 }
