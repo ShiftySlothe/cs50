c77b6b9b243201a8c100ed8d6406ae59642eee5e
sHIFTYsLOTHE <sHIFTYsLOTHE@users.noreply.github.com>
2021-03-01 19:11:15 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -11,10 +11,11 @@ float CalculateReadability();
 int main(void)
 {
     userInput = get_string("Text: ");
+    
     float unroundedScore = CalculateReadability();
     int roundedScore = roundf(unroundedScore);
 
-    if(roundedScore < 1)
+    if (roundedScore < 1)
     {
         printf("Before Grade 1\n");
     }
@@ -31,6 +32,7 @@ int main(void)
 float CalculateReadability()
 {
     float letterCount = 0;
+    //Initialised at 1 to account for the final word
     float wordCount = 1;
     float sentenceCount = 0;
 
@@ -39,27 +41,30 @@ float CalculateReadability()
         char currentChar = userInput[i];
         currentChar = toupper(currentChar);
         int currentCharIndex = currentChar - 0;
-
-        if(currentCharIndex >= 65 && currentCharIndex <= 90)
+        
+        //Checks to see if the char is within the ASCII bounds of A - Z
+        if (currentCharIndex >= 65 && currentCharIndex <= 90)
         {
             letterCount++;
         }
-        if(currentCharIndex == 32)
+        //Checks for a space
+        if (currentCharIndex == 32)
         {
             wordCount++;
         }
-
-        if(currentCharIndex == 33 || currentCharIndex == 46 || currentCharIndex == 63)
+        //Checks for ! . ?
+        if (currentCharIndex == 33 || currentCharIndex == 46 || currentCharIndex == 63)
         {
             sentenceCount++;
         }
-
     }
-
     
     float wordsPer100 = wordCount / 100;
+    //Number of letters per 100 words
     float lettersPer100 = letterCount / wordsPer100;
+    //Number of sentences per 100 words
     float sentencesPer100 = sentenceCount / wordsPer100;
     
+    //Coleman-Liau index
     return (0.0588 * lettersPer100) - (0.296 * sentencesPer100) - 15.8;
 }
