b554a8c230ef43e5f4cbaccad9af9c221cde74e0
ShiftySlothe <ShiftySlothe@users.noreply.github.com>
2021-03-08 11:05:48 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -6,10 +6,11 @@ int main(void)
     int sizeOfTower;
     
     //Requests input until corect 
-    do{
-      sizeOfTower = get_int("How tall do you want your tower? (1 - 8)");
+    do
+    {
+        sizeOfTower = get_int("How tall do you want your tower? (1 - 8)");
     }
-    while(sizeOfTower > 8 || sizeOfTower < 1);
+    while (sizeOfTower > 8 || sizeOfTower < 1);
 
     //For each row
     for (int i = 1; i <= sizeOfTower; i++)
@@ -20,14 +21,14 @@ int main(void)
             printf(" ");
         }
         //Draw blocks
-        for(int k = i; k > 0; k--)
+        for (int k = i; k > 0; k--)
         {
             printf("#");
         }
         //Print 2 spaces 
         printf("  ");
         //Draw same number of blocks
-        for(int l = i; l > 0; l--)
+        for (int l = i; l > 0; l--)
         {
             printf("#");
         }
