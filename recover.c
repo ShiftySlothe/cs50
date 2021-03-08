67b014bd4ea3587a99cf50a51d9b7f28ec0d39fe
ShiftySlothe <ShiftySlothe@users.noreply.github.com>
2021-03-08 12:54:52 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -22,40 +22,29 @@ int main(int argc, char *argv[])
         return 1;
     }
     
-    //Try seek end of file
-    if (fseek(inputFile, 0L, SEEK_END) != 0)
-    {
-        printf("File read error, exiting");
-        return 1;
-    }
     //Create buffer to store block
     Byte buffer[512];
     
-    int numberOfJPEG = -1;
-    char *currentFileName = "000";
+    int numberOfJPEG = 0;
+    char currentFileName[8];
+    FILE *img = NULL;
     bool jpegFound = false;
-    FILE *img = fopen(currentFileName, "w");
+    
     //Read the file to buffer
-    while (fread(buffer, 512, 1, inputFile) == 512)
+    while (fread(&buffer, 512, 1, inputFile) == 1)
     {
-        //Gets only the first 4 of the 4th buffer, setting last to 0
-        Byte firstFourFourthBuffer = buffer[3] & 0xf0;
-        
-        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && firstFourFourthBuffer == 0xe0)
+        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            jpegFound = true;
-            numberOfJPEG++;
-            if(numberOfJPEG == 0)
-            {
-                continue;
-            }
-            else
+            if(numberOfJPEG != 0)
             {
                 fclose(img);
-                sprintf(currentFileName, "%03i", numberOfJPEG);
-                img = fopen(currentFileName, "w");
             }
+            sprintf(currentFileName, "%03i.jpg", numberOfJPEG);
+            img = fopen(currentFileName, "w");
+            numberOfJPEG++;
+            jpegFound = true;
         }
+        
         if(jpegFound)
             fwrite(buffer, 512, 1, img);
     }
